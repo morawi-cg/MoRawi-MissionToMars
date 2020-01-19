ea5e44043654eb58ecd86c98434d61354a632261
Software Engineer, Virtualization specialist <morawi-cg@users.noreply.github.com>
2020-01-19 17:41:50 +0000

    Create mysql_config_linux.sh
    Added this bash based file to create the mysql database and user credentials. Can be adapted for container, virtual machine and physical host. General steps can be adapted to poweshell.

---
+++ mysql_config_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+CREATE DATABASE `todoapp`;
+CREATE USER 'guest' IDENTIFIED BY 'Guest0000!';
+GRANT USAGE ON `todoapp`.* TO 'guest'@localhost IDENTIFIED BY 'Guest0000!';
+GRANT ALL privileges ON `todoapp`.* TO 'guest'@localhost;
+FLUSH PRIVILEGES;
