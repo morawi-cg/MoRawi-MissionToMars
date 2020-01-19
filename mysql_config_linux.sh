2bb1c2de87d747c14cae1dffacc614c2e317694f
Software Engineer, Virtualization specialist <morawi-cg@users.noreply.github.com>
2020-01-19 18:06:34 +0000

    Update mysql_config_linux.sh
    Added the component that created the db components on a mysql/mariadb server.

--- mysql_config_linux.sh
+++ mysql_config_linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mysql -u root -p
 CREATE DATABASE `todoapp`;
 CREATE USER 'guest' IDENTIFIED BY 'Guest0000!';
 GRANT USAGE ON `todoapp`.* TO 'guest'@localhost IDENTIFIED BY 'Guest0000!';
