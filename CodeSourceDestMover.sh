ef2929a6ca2073fa494152f9e87879745ed7e450
Mo Rawi <engineer@localdomain>
2020-02-16 13:11:36 -0500

    changed the code to allow the CodeSourceDestMover.sh to extend it function to building the images

--- CodeSourceDestMover.sh
+++ CodeSourceDestMover.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Thi script is tasked with copying the contents over to the target folders, the ADD and COPY functions, associated with the Dockerfile syntax tend to fail if source is outside the work folder.
 cp -p ../booster B
+
+
+docker build --squash -t morawi/missiontomarstodoapp-db:latest -f todoapp-db/Dockerfile 
+docker build --squash  -t morawi/missiontomarsbooster:latest -f Booster/Dockerfile
