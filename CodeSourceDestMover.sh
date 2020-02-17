f2cb0186d5998b832a45b8380d857b112dd4af66
Mo Rawi <engineer@localdomain>
2020-02-17 00:10:11 -0500

    changes to the docker-compose.yaml

--- CodeSourceDestMover.sh
+++ CodeSourceDestMover.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Thi script is tasked with copying the contents over to the target folders, the ADD and COPY functions, associated with the Dockerfile syntax tend to fail if source is outside the work folder.
-cp -p ../booster B
+cp -p ../../booster Booster/
 
 
-docker build --squash -t morawi/missiontomarstodoapp-db:latest -f todoapp-db/Dockerfile 
-docker build --squash  -t morawi/missiontomarsbooster:latest -f Booster/Dockerfile
+docker build  -t morawi/missiontomarstodoapp-db:latest -f todoapp-db/Dockerfile 
+docker build  -t morawi/missiontomarsbooster:latest -f Booster/Dockerfile
